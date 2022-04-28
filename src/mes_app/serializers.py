e5e57d233ea81f36e7381bf806cd66838b27c0f3
Mikhail <mixail.critsyn@mail.ru>
2022-04-28 13:51:05 +0300

    add serializers for sent and recieve

--- src/mes_app/serializers.py
+++ src/mes_app/serializers.py
@@ -1,13 +1,28 @@
 from rest_framework import serializers
 
 from src.mes_app.models import Message
+from src.profiles.serializers import GetPublicMessageUserSerializer
 
 
-class MessageCreateSerializer(serializers.Serializer):
+class MessageCreateReadSerializer(serializers.Serializer):
     text = serializers.CharField(max_length=5000)
 
 
-class GetMessageSerializer(serializers.ModelSerializer):
+class GetMessageSendSerializer(serializers.ModelSerializer):
+    """ Serializer for sent messages
+    """
+    recipient = GetPublicMessageUserSerializer()
+
     class Meta:
         model = Message
-        fields = ('__all__')
+        exclude = ('key', 'sender')
+
+
+class GetMessageReceiveSerializer(serializers.ModelSerializer):
+    """ Serializer for received messages
+    """
+    sender = GetPublicMessageUserSerializer()
+
+    class Meta:
+        model = Message
+        exclude = ('key', 'recipient')
